a7e617636aee933c42bfb09e5482a639b76f578a
Martin <haenschmartin@gmail.com>
2022-07-08 21:39:04 -0700

    firebase login

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
@@ -8,10 +9,18 @@ const SignIn = () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log({ user });
+  };
   return (
     <div>
       <h1> SIGN IN PAGE</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sign in with GoogleRedirect
+      </button>
     </div>
   );
 };
